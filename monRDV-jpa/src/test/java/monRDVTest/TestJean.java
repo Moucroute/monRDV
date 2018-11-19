3f96e578619973998b0ec6455c2f7dd490650c30
jean DUHEM <jeandelens@msn.com>
2018-11-19 17:07:27 +0100

    Test Jean

--- monRDV-jpa/src/test/java/monRDVTest/TestJean.java
+++ monRDV-jpa/src/test/java/monRDVTest/TestJean.java
@@ -1,17 +1,19 @@
 package monRDVTest;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
 import monRDV.dao.IDaoUtilisateur;
 import monRDV.dao.jpa.DaoUtilisateurJpa;
+import monRDV.model.Profil;
 import monRDV.model.Utilisateur;
 
 
 
 public abstract class TestJean {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
@@ -28,34 +30,40 @@ public abstract class TestJean {
 		utilisateur1.setTelephone("0669696969");
 		utilisateur1.setDateCreation(sdf.parse("25/12/2000"));
 		utilisateur1.setMotDePasse("liugfhlkjs<fhKsuil<");
-		utilisateur1.setProfil(profil);
+		utilisateur1.setProfil(Profil.Patient);
 		
 		
 		utilisateur1 = daoUtilisateur.save(utilisateur1); // managed
 
-		Utilisateur utilisateur1Find = daoUtilisateur.find(utilisateur1.getNom());
+		Utilisateur utilisateur1Find = daoUtilisateur.find(utilisateur1.getId());
 
-		System.out.println(utilisateur1.getNom() + "=" + matiere1Find.getNom());
-		System.out.println(utilisateur1.getDuree() + "=" + matiere1Find.getDuree());
-		System.out.println(utilisateur1.getNiveau() + "=" + matiere1Find.getNiveau());
-
+		System.out.println(utilisateur1.getId() + "=" + utilisateur1Find.getId());
+		System.out.println(utilisateur1.getEmail() + "=" + utilisateur1Find.getEmail());
+		System.out.println(utilisateur1.getTelephone() + "=" + utilisateur1Find.getTelephone());
+		System.out.println(utilisateur1.getDateCreation() + "=" + utilisateur1Find.getDateCreation());
+		System.out.println(utilisateur1.getMotDePasse() + "=" + utilisateur1Find.getMotDePasse());
+		System.out.println(utilisateur1.getProfil() + "=" + utilisateur1Find.getProfil());
+		
 		utilisateur1 = daoUtilisateur.save(utilisateur1); // managed
 
-		matiere1Find = daoUtilisateur.find(utilisateur1.getNom());
+		utilisateur1Find = daoUtilisateur.find(utilisateur1.getId());
 
-		System.out.println(utilisateur1.getNom() + "=" + matiere1Find.getNom());
-		System.out.println(utilisateur1.getDuree() + "=" + matiere1Find.getDuree());
-		System.out.println(utilisateur1.getNiveau() + "=" + matiere1Find.getNiveau());
+		System.out.println(utilisateur1.getId() + "=" + utilisateur1Find.getId());
+		System.out.println(utilisateur1.getEmail() + "=" + utilisateur1Find.getEmail());
+		System.out.println(utilisateur1.getTelephone() + "=" + utilisateur1Find.getTelephone());
+		System.out.println(utilisateur1.getDateCreation() + "=" + utilisateur1Find.getDateCreation());
+		System.out.println(utilisateur1.getMotDePasse() + "=" + utilisateur1Find.getMotDePasse());
+		System.out.println(utilisateur1.getProfil() + "=" + utilisateur1Find.getProfil());
 		
 		utilisateurs = daoUtilisateur.findAll();
 
 		System.out.println("Nombre avant la suppression="+utilisateurs.size());
 		
-//		daoMatiere.delete(matiere1);
+//		daoUtilisateur.delete(utilisateur1);
 //		
-//		matiere1Find = daoMatiere.find(matiere1.getNom());
+//		utilisateur1Find = daoUtilisateur.find(utilisateur1.getId());
 //		
-//		System.out.println(matiere1Find);
+//		System.out.println(utilisateur1Find);
 	}
 
 }
