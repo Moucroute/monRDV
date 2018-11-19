f098c86ee1b862162ad44843c836485d41144600
jory <jory.labat@gmail.com>
2018-11-19 15:11:07 +0100

    ajout Class:  Lieu Adresse CreneauDisponible

---
+++ monRDV-model/src/main/java/monRDV/model/CreneauDisponible.java
@@ -0,0 +1,69 @@
+package monRDV.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "creneauDisponible")
+public class CreneauDisponible {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private int version;
+	private Date debut;
+	private Date fin;
+	@ManyToOne
+	@JoinColumn(name = "lieu_id")
+	private Lieu lieu;
+
+	public CreneauDisponible() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public Date getDebut() {
+		return debut;
+	}
+
+	public void setDebut(Date debut) {
+		this.debut = debut;
+	}
+
+	public Date getFin() {
+		return fin;
+	}
+
+	public void setFin(Date fin) {
+		this.fin = fin;
+	}
+
+	public Lieu getLieu() {
+		return lieu;
+	}
+
+	public void setLieu(Lieu lieu) {
+		this.lieu = lieu;
+	}
+
+}
