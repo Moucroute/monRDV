7ce6244e4c23edd7e86a4a956de52cbf62ee3dec
Simon V <simon.varvat@gmail.com>
2018-11-19 17:03:41 +0100

    Les imports qui vont bien

--- monRDV-model/src/main/java/monRDV/model/Patient.java
+++ monRDV-model/src/main/java/monRDV/model/Patient.java
@@ -5,14 +5,11 @@ import java.util.Date;
 import java.util.List;
 
 import javax.persistence.Column;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
-import javax.persistence.MappedSuperclass;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
--- monRDV-model/src/main/java/monRDV/model/RendezVous.java
+++ monRDV-model/src/main/java/monRDV/model/RendezVous.java
@@ -6,16 +6,12 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToOne;
-import javax.persistence.MappedSuperclass;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.Version;
-
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.Version;
 
 
 
