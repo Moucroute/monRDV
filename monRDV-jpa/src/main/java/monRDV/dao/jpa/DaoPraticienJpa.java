dd5d53901bce02601011195163e02d484c10db69
Simon V <simon.varvat@gmail.com>
2018-11-20 11:26:51 +0100

    Auto stash before merge of "master" and "origin/master"

--- monRDV-jpa/src/main/java/monRDV/dao/jpa/DaoPraticienJpa.java
+++ monRDV-jpa/src/main/java/monRDV/dao/jpa/DaoPraticienJpa.java
@@ -137,10 +137,15 @@ public class DaoPraticienJpa implements IDaoPraticien {
 			tx = em.getTransaction();
 			tx.begin();
 
-			Query query = null;
-			
+			TypedQuery<Praticien> query = em.createQuery("select p from Praticien p join fetch p.lieux l where p.nom = :param1 and where l.nom = :param2", Praticien.class);
+			query.setParameter("param1", nom);
+			query.setParameter("param2", lieu);
+//			query.setParameter("param3", libelle);
+			list = query.getResultList();
 			if ((nom != null)&&(lieu != null)&&(libelle != null)) {
 				query = em.createQuery("select p from praticien p left join fetch p.specialite left join fetch p.lieux where p.nom = :param1 and :param2 member of p.lieux.nom and :param3 member of p.specialites.libelle", Praticien.class);
+			
+			Query query = null;
 
 			}
 			
