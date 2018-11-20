e8b0f2b2539db1f9a5659a69c74a165c6452281b
Simon V <simon.varvat@gmail.com>
2018-11-20 11:25:15 +0100

    test simon

--- monRDV-jpa/src/test/java/monRDVTest/monRDVTestJPASimon.java
+++ monRDV-jpa/src/test/java/monRDVTest/monRDVTestJPASimon.java
@@ -1,11 +1,16 @@
 package monRDVTest;
 
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+
+import monRDV.dao.IDaoPraticien;
+import monRDV.dao.jpa.DaoPraticienJpa;
+import monRDV.model.Praticien;
 
 public class monRDVTestJPASimon {
 
@@ -14,6 +19,8 @@ public class monRDVTestJPASimon {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("monRDV");
 		EntityManager em = null;
 		EntityTransaction tx = null;
+		IDaoPraticien daoPraticien = new DaoPraticienJpa();
+		List<Praticien> praticiens = new ArrayList<>();
 
 		
 		try {
@@ -21,6 +28,16 @@ public class monRDVTestJPASimon {
 			tx = em.getTransaction();
 			
 			tx.begin();
+			
+			praticiens = daoPraticien.findByNomLieuSpecialite("Mouden","Cabinet Caramel","proctologie");
+			
+			for (Praticien praticien : praticiens) {
+				System.out.println(praticien.getId());
+				System.out.println(praticien.getNom());
+				System.out.println(praticien.getPrenom());
+				System.out.println(praticien.getPrendCarteVitale());
+			}
+			
 			
 			tx.commit(); 
 			
